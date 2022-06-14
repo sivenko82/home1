320d50a80ae312867ce8c0d135afd71394dec1a2
sivenko82 <sivenko822@gmail.com>
2022-06-14 21:29:38 +0300

    less PET'

--- OOP/OOP3.py
+++ OOP/OOP3.py
@@ -1,64 +1,43 @@
-class Dog:
+class Pet:
     def __init__(self, name, age, master):
-        self.name=name
-        self.age=age
-        self.master=master
-    def jump(self):
-        return   f"прыжок {self.name} на м"
-    def run(self,):
-        return f'сокрость бега {self.name} км в ч '
-    def birthday(self,birthday):
-         return  self.age+1 ,"возраст"
-    def bark(self):
-        return f" лаять"
-
-class Cat:
-    def __init__(self,name,age,master):
-        self.name=name
-        self.age=age
-        self.master=master
-    def run(self,run):
-        return "кошка бежит"
-    def jump(self,jump):
-        return "прыжок"
-    def birthday(self,birthday):
-         return  self.age+1 ,"возраст"
-    def sleep(self,sleep):
-        return f"{sleep} спит "
-    def meow(self):
-        return f"мяу"
-
-class Parrot:
-
-    def __init__(self,name,age,master):
         self.name = name
         self.age = age
         self.master = master
-    def run(self, run):
-        return "попугай бежит"
-    def jump(self, jump):
-        return " попугай прыжок"
-    def birthday(self, birthday):
-        return self.age + 1, "возраст попугая"
-    def sleep(self,sleep):
-        return f"{sleep} спит "
+    def run(self,):
+        return f"бежит{self.name}"
+    def jump(self,):
+        return "  прыжок"
+    def birthday(self,):
+        return self.age + 1, " возраст"
+    def sleep(self,):
+        return f"{self.name} спит "
+
+
+class Dog(Pet):
+    def __init__(self, name, age, master):
+        super().__init__(name,age,master)
+    def bark(self):
+        return f" лаять"
+if __name__=="__main__":
+    dogs=Dog("BOB",12,20)
+    print(dogs.run(),dogs.bark(),dogs.jump(),dogs.birthday(),dogs.sleep(),sep="\n")
+
+
+class Cat(Pet):
+    def __init__(self, name, age, master):
+        super().__init__(name,age,master)
+    def meow(self):
+        return f"мяу"
+if __name__=="__main__":
+    dogs=Cat("Catt",2,222)
+    print(dogs.run(),dogs.meow(),dogs.jump(),dogs.birthday(),dogs.sleep(),sep="\n")
+
+
+class Parrot(Pet):
+    def __init__(self, name, age, master):
+        super().__init__(name,age,master)
     def fly(self):
         return f"летать"
-class Pet(Dog,Cat,Parrot):
-    def __init__(self,master, age, name ):
-        super().__init__(name, age, master)
-    def run(self, run):
-        return "бежит"
-    def jump(self, jump):
-        return "  прыжок"
-    def birthday(self, birthday):
-        return self.age + 1, " попугая"
-    def sleep(self,sleep):
-        return f"{sleep} спит "
-    def Del(self):
-        del self.name,self.age,self.master
-
 if __name__=="__main__":
-    dogs=Pet(60,12,"vita")
-    print(dogs.Del())
-    print(dogs.bark()  ,dogs.fly(),dogs.meow(),sep="\n")
+    dogs=Parrot("Kesha",14,50)
+    print(dogs.run(),dogs.fly(),dogs.jump(),dogs.birthday(),dogs.sleep(),sep="\n")
