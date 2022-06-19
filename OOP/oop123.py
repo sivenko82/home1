2ce7b42853e35e2ce567022ba04ac7b21a8ff30d
sivenko82 <sivenko822@gmail.com>
2022-06-19 13:48:34 +0300

    del

--- OOP/oop123.py
+++ OOP/oop123.py
@@ -4,16 +4,14 @@ class BeautySalonMixin:
         self.open=open
         self.close=close
     def manic(self):
-        a=int(BeautySalonMixin.price)
-        return f"маник стоит {a+(a*0.20)} руб"
+        return f"маник стоит {BeautySalonMixin.price+(BeautySalonMixin.price*0.20)} руб"
     def haircut(self,long):
-        wor=int(BeautySalonMixin.price)
         if long<30:
-            return f"стрижка стоит {wor+(wor*0.20)}"
+            return f"стрижка стоит {BeautySalonMixin.price+(BeautySalonMixin.price*0.20)}"
         elif 30<=long<=50:
-            return f"стрижка стоит {wor+(wor*0.50)}"
+            return f"стрижка стоит {BeautySalonMixin.price+(BeautySalonMixin.price*0.50)}"
         else:
-            return f"стрижка стоит {wor+(wor*0.80)}"
+            return f"стрижка стоит {BeautySalonMixin.price+(BeautySalonMixin.price*0.80)}"
 
     def salon_opening_hours(self, time):
         if self.close> time > self.open:
@@ -26,4 +24,4 @@ class BeautySalonMixin:
 
 if __name__ == '__main__':
     a=BeautySalonMixin(10,20)
-    print(a.haircut(int(input())))
+    print(a.haircut(10))
